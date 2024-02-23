99f50353db6ba500d428ffab8d7ff1a096986e04
Raul Lira <raullirgue@alum.us.es>
2024-02-23 12:22:52 +0100

    Tabal Restaurants1

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -6,11 +6,69 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
+      },
+      restaurantCategoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'RestaurantCategories'
+          },
+          key: 'id'
+        }
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      postalCode: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      url: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      shippingCosts: {
+        allowNull: false,
+        type: Sequelize.DOUBLE
+      },
+      averageServiceMinutes: {
+        allowNull: true,
+        type: Sequelize.DOUBLE
+      },
+      email: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      phone: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      logo: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      heromage: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      states: {
+        type: Sequelize.ENUM,
+        values: ['online', 'offline', 'closed', 'temporally closed']
       }
       // TODO: Include the rest of the fields of the Restaurants table
 
     })
   },
+
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Restaurants')
   }
